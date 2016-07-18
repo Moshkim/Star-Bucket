44ab0fb49d179b0edb247e240810f4600024bbfa
Moses <rlakim@ucdavis.edu>
2016-07-18 16:10:16 +0900

    cocoapod initialization

---
+++ Pods/Target Support Files/Pods-star&bucket/Pods-star&bucket-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_star_bucket : NSObject
+@end
+@implementation PodsDummy_Pods_star_bucket
+@end
---
+++ Pods/Target Support Files/Pods-star&bucket/Pods-star&bucket-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_star_bucketVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_star_bucketVersionString[];
+
